533d9dad363eb5a15fd729b41f393c458c188bb2
Mudit Satija <mudit.satija@scryanalytics.com>
2021-08-31 19:11:16 +0530

    search functionality integration with new UI

--- groundx-angular-web/src/main/app/app/common/subHeader/subHeader.component.ts
+++ groundx-angular-web/src/main/app/app/common/subHeader/subHeader.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { Search } from '../../search';
 
 @Component({
   selector: 'app-sub-header',
+  providers: [Search],
   templateUrl: './subHeader.component.html',
   styleUrls: ['./subHeader.component.css']
 })
