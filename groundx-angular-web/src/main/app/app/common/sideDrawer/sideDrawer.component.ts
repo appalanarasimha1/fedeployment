b347b59a0c0f45297cbfbea386505e1d4ad64f10
Shubhanshi Verma <shubhanshi.verma@scryanalytics.com>
2021-09-03 11:01:36 +0530

    header, sub-header, sidebar according to new ui

--- groundx-angular-web/src/main/app/app/common/sideDrawer/sideDrawer.component.ts
+++ groundx-angular-web/src/main/app/app/common/sideDrawer/sideDrawer.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-side-drawer',
   templateUrl: './sideDrawer.component.html',
-  styleUrls: ['./sideDrawer.component.css']
+  styleUrls: ['./sideDrawer.component.css'],
+
 })
 export class SideDrawerComponent {
 openNav() {
-    document.getElementById("main-sidebar").style.width = "270px";
+    document.getElementById("main-sidebar").style.width = "280px";
     document.getElementById("main").classList.toggle('shiftFilter');
   document.getElementById("main-sidebar").classList.toggle("closeBtn");
 
