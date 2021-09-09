67c46302dbb9ab66dec8730d0f2be88c28d49eb9
Shubhanshi Verma <shubhanshi.verma@scryanalytics.com>
2021-09-09 18:32:24 +0530

    ng bootstrap added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 // import { SideDrawerComponent } from './common/sideDrawer/sideDrawer.component';
 
 @NgModule({
@@ -13,7 +14,8 @@ import { HeaderComponent } from './common/header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
