bca59569ab65e7634e99954e89592d5dcef8c332
Mudit Satija <mudit.satija@scryanalytics.com>
2021-10-16 22:57:17 +0530

    prep for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,8 +6,8 @@ export const environment = {
   // apiVersion: '/nuxeo/api/v1'
 
   production: true,
-  baseUrl: 'https://uatgroundx.neom.com',
+  nuxeoServiceBaseUrl: 'https://uatgroundx.neom.com',
   redirectBaseUrl: 'https://10.101.21.63:8087',
-  apiBaseUrl: 'https://dev1groundx.neom.com',
+  apiServiceBaseUrl: 'https://dev1groundx.neom.com',
   apiVersion: '/nuxeo/api/v1'
 };
