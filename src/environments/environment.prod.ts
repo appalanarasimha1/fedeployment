71ea92d49c666538b6b7584e76922acb94a41389
Mudit Satija <mudit.satija@scryanalytics.com>
2021-10-16 22:35:48 +0530

    prep for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,13 @@
 export const environment = {
-  production: false,
-  nuxeoServiceBaseUrl: 'http://10.101.21.31:8080',
-  redirectBaseUrl: 'https://devtomcat-groundx.neom.com:8090/', // for redirecting to old nuxeo ui for video playback
-  apiServiceBaseUrl: 'http://10.101.21.31:8080',
+  // production: true,
+  // nuxeoServiceBaseUrl: 'http://10.101.21.31:8080',
+  // redirectBaseUrl: 'https://devtomcat-groundx.neom.com:8090/', // for redirecting to old nuxeo ui for video playback
+  // apiServiceBaseUrl: 'http://10.101.21.31:8080',
+  // apiVersion: '/nuxeo/api/v1'
+
+  production: true,
+  baseUrl: 'https://uatgroundx.neom.com',
+  redirectBaseUrl: 'https://10.101.21.63:8087',
+  apiBaseUrl: 'https://dev1groundx.neom.com',
   apiVersion: '/nuxeo/api/v1'
 };
