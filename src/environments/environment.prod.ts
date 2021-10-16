183d2179daa12141303c5cd3549b6afa69ea7621
Mudit Satija <mudit.satija@scryanalytics.com>
2021-10-16 19:45:16 +0530

    api url changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  nuxeoServiceBaseUrl: 'https://devtomcat-groundx.neom.com:8090',
-  redirectBaseUrl: 'http://10.101.21.31:8080/', // for redirecting to old nuxeo ui for video playback
-  apiServiceBaseUrl: 'https://devtomcat-groundx.neom.com:8090',
+  nuxeoServiceBaseUrl: 'http://10.101.21.31:8080',
+  redirectBaseUrl: 'https://devtomcat-groundx.neom.com:8090/', // for redirecting to old nuxeo ui for video playback
+  apiServiceBaseUrl: 'http://10.101.21.31:8080',
   apiVersion: '/nuxeo/api/v1'
 };
