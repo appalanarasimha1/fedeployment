5156fe14fe74aac68f0d65a66868c9d9eecf2f61
unknown <mudit.satija@scryanalytics.com>
2021-10-29 13:16:52 +0530

    500 post error fix

--- server/app.ts
+++ server/app.ts
@@ -12,10 +12,10 @@ export class App {
 
   constructor() {
     this.app = express();
-    // parse application/x-www-form-urlencoded
-    this.app.use(bodyParser.urlencoded({ extended: false }));
-    // parse application/json
-    this.app.use(bodyParser.json());
+    // // parse application/x-www-form-urlencoded
+    // this.app.use(bodyParser.urlencoded({ extended: false }));
+    // // parse application/json
+    // this.app.use(bodyParser.json());
     this.app.use(morgan('dev'));
     this.app.use(cors());
     const httpsOptions = {
--- server/routeManager.ts
+++ server/routeManager.ts
@@ -1,6 +1,7 @@
 import * as path from 'path';
 import * as express from 'express';
 import { createProxyMiddleware, responseInterceptor } from 'http-proxy-middleware';
+import bodyParser from 'body-parser';
 
 const allowedExt = [
   '.js',
@@ -39,33 +40,36 @@ export class RouteManager {
       changeOrigin: true, // needed for virtual hosted sites
       ws: true, // proxy websockets
       secure: false, // if you want to verify the certificate
-      
-      onProxyRes: responseInterceptor(async (responseBuffer, proxyRes: any, req, res: any) => {
-        // const response = responseBuffer.toString('utf8'); // convert buffer to string
-        try {
-          // console.log('res = ', proxyRes.statusCode);
-          if (res.statusCode === 401) {
-            res.statusCode = 302;
-            proxyRes.headers['location'] = 'http://uatgroundx.neom.com/login';
-          }
-          return responseBuffer;
-        } catch (e) {
-          console.error('error = ', e);
-          return responseBuffer;
-        }
-      })
+
+      // onProxyRes: responseInterceptor(async (responseBuffer, proxyRes: any, req, res: any) => {
+      //   // const response = responseBuffer.toString('utf8'); // convert buffer to string
+      //   try {
+      //     // console.log('res = ', proxyRes.statusCode);
+      //     if (res.statusCode === 401) {
+      //       res.statusCode = 302;
+      //       proxyRes.headers['location'] = 'http://uatgroundx.neom.com/login';
+      //     }
+      //     return responseBuffer;
+      //   } catch (e) {
+      //     console.error('error = ', e);
+      //     return responseBuffer;
+      //   }
+      // })
     };
 
     this.app.use('/nuxeo/', createProxyMiddleware(
       {
         target: this.targetUrl,
         changeOrigin: true,
-        secure: false,
+        // secure: false,
         logLevel: 'debug',
 
       }
     ));
-
+    // parse application/x-www-form-urlencoded
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+    // parse application/json
+    this.app.use(bodyParser.json());
     // Default route.
     this.app.use(express.static(path.resolve(__dirname + '/../../' + '/dist')));
     this.app.get('*', (req: express.Request, res: express.Response) => {
