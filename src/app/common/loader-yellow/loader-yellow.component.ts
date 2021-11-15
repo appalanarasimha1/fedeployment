fce88b3f9911e03fcd62f276a8fe038b3a140a92
Shubhanshi Verma <shubhanshi.verma@scryanalytics.com>
2021-11-15 20:41:20 +0530

    loader and browse screen done

---
+++ src/app/common/loader-yellow/loader-yellow.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loader-yellow',
+  templateUrl: './loader-yellow.component.html',
+  styleUrls: ['./loader-yellow.component.css']
+})
+export class LoaderYellowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
