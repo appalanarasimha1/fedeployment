17754b2853a215cd5b410ee754b76cca62a4aa6e
unknown <mudit.satija@scryanalytics.com>
2021-11-25 19:29:09 +0530

    api fix

--- server/routeManager.ts
+++ server/routeManager.ts
@@ -1,7 +1,10 @@
 import * as path from 'path';
 import * as express from 'express';
 import { createProxyMiddleware, responseInterceptor } from 'http-proxy-middleware';
+import { ConnectionFactory } from './connectionManager/ConnectionFactory';
+import { DbConnection } from './connectionManager/DbConnection';
 import bodyParser from 'body-parser';
+import { PersonalizedVideoController } from './controllers/personalize-video.controller';
 
 const allowedExt = [
   '.js',
@@ -18,10 +21,32 @@ const allowedExt = [
 
 export class RouteManager {
   private readonly app: any;
-  private targetUrl = process.env.API_SERVER;
-
+  private targetUrl;
   constructor(app: any) {
     this.app = app;
+
+    switch (process.env.NODE_ENV) {
+      case 'dev':
+        this.targetUrl = 'https://10.101.21.31:8090';
+        // this.port = 5050;
+        // this.protocol = '';
+        break;
+      case 'demo':
+        this.targetUrl = 'https://34.219.179.33:5050/';
+        // this.port = 5050;
+        // this.protocol = 'https:';
+        break;
+      case 'production':
+        this.targetUrl = 'https://34.219.179.33:5050/';
+        // this.port = 8087;
+        // this.protocol = 'https:';
+        break;
+      default:
+        this.targetUrl = 'https://34.219.179.33:5050/';
+        // this.port = 5050;
+        // this.protocol = 'http:';
+        break;
+    }
     this.mountRoutes(app);
   }
 
@@ -29,28 +54,50 @@ export class RouteManager {
    * Function to mount all the application routes under express application.
    */
   private mountRoutes(app: any): void {
+    // Other routes.
+    // this.app.use('/api/v1/user', UserController.Instance.Router);
+    // this.app.use('/api/v1/device-management', checkApiAuth, DeviceManagementController.Instance.Router);
+    this.app.use('/nuxeo/api/v1/fetchPersonalizedVideo', PersonalizedVideoController.Instance.Router);
+    initialiseServices(app);
+    
+    // this.app.use('/nuxeo/api/v1/fetchPersonalizedVideo', (req: any, res: any) => {
+    //   console.log('fetchPersonalizedVideo');
+    //   /**
+    //    * fetch user details from userDirectory
+    //    * in assetSeen: {sector: sectorname, videoSeen: ['_id of videos']}
+    //    * search in video_processing table {sector: assetSeen.sector, _id: {$nin: assetSeen.videoSeen}}
+    //    * if result found
+    //    *    send 1st object
+    //    *    update assetSeen with _id of video
+    //    * else
+    //    *    find video of specified sector from video_processing table  {sector: assetSeen.sector}, {limit: 1}
+    //    *    return result
+    //    */
+    //   res.send({message: 'done', status: 200});
+    //   return;
+    // });
 
-    // const options = {
-    //   target: this.targetUrl, // target host
-    //   changeOrigin: true, // needed for virtual hosted sites
-    //   ws: true, // proxy websockets
-    //   secure: false, // if you want to verify the certificate
-
-    //   onProxyRes: responseInterceptor(async (responseBuffer, proxyRes: any, req, res: any) => {
-    //     // const response = responseBuffer.toString('utf8'); // convert buffer to string
-    //     try {
-    //       // console.log('res = ', proxyRes.statusCode);
-    //       if (res.statusCode === 401) {
-    //         res.statusCode = 302;
-    //         proxyRes.headers['location'] = 'http://uatgroundx.neom.com/login';
-    //       }
-    //       return responseBuffer;
-    //     } catch (e) {
-    //       console.error('error = ', e);
-    //       return responseBuffer;
-    //     }
-    //   })
-    // };
+    const options = {
+      target: this.targetUrl, // target host
+      changeOrigin: true, // needed for virtual hosted sites
+      ws: true, // proxy websockets
+      secure: false, // if you want to verify the certificate
+      
+      // onProxyRes: responseInterceptor(async (responseBuffer, proxyRes: any, req, res: any) => {
+      //   // const response = responseBuffer.toString('utf8'); // convert buffer to string
+      //   try {
+      //     // console.log('res = ', proxyRes.statusCode);
+      //     if (res.statusCode === 401) {
+      //       res.statusCode = 302;
+      //       proxyRes.headers['location'] = 'http://uatgroundx.neom.com/login';
+      //     }
+      //     return responseBuffer;
+      //   } catch (e) {
+      //     console.error('error = ', e);
+      //     return responseBuffer;
+      //   }
+      // })
+    };
 
     this.app.use('/nuxeo/', createProxyMiddleware(
       {
@@ -58,12 +105,15 @@ export class RouteManager {
         changeOrigin: true,
         secure: false,
         logLevel: 'debug',
+
       }
     ));
+    
     // parse application/x-www-form-urlencoded
     this.app.use(bodyParser.urlencoded({ extended: false }));
     // parse application/json
     this.app.use(bodyParser.json());
+
     // Default route.
     this.app.use(express.static(path.resolve(__dirname + '/../../' + '/dist')));
     this.app.get('*', (req: express.Request, res: express.Response) => {
@@ -73,6 +123,26 @@ export class RouteManager {
         res.sendFile(path.resolve(__dirname + '/../../' + '/dist/index.html'));
       }
     });
+
+    // function checkApiAuth(req: express.Request | any, res: express.Response, next: express.NextFunction) {
+    //   const token: string = req.headers['access-token'];
+    //   if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+
+    //   const secret = process.env.SECRET;
+    //   jwt.verify(token, secret, function (err, decoded) {
+    //     if (err) return res.status(402).send({ auth: false, message: 'Failed to authenticate token.' });
+
+    //     next();
+    //   });
+    // }
+
+    
+    async function initialiseServices(app: any) {
+      // innitialising mongo connection
+      let connectionManager: DbConnection = ConnectionFactory.getConnectionManager();
+      const connection = await connectionManager.getConnection();
+      return;
+    }
 
   }
 }
