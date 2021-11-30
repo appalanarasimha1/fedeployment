037869ec515260447dc36d31629f211d2eef3e12
unknown <mudit.satija@scryanalytics.com>
2021-11-30 14:15:40 +0530

    prod db and user change

--- server/config/appConfig.production.ts
+++ server/config/appConfig.production.ts
@@ -1,12 +1,12 @@
 export class AppConfigProduction {
-  private static userName: string = 'dev_user';
+  private static userName: string = 'gxproduser';
   private static password: any = process.env.mongoPassword;
 
   private static config: any = {
     port: 4001,
     socketPort: 4010,
     mongoDbUrl: `mongodb://${encodeURIComponent(this.userName)}:${encodeURIComponent(this.password)}@mongodb1.core-dev.neos.today:27017`,
-    dbName: 'dev_groundx',
+    dbName: 'prodgx',
     mongodbTables: {
       USER_TABLE: 'userDirectory',
       VIDEO_TABLE: 'personalizedVideoInventory'
