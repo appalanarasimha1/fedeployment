89d5cb6d287f321934fe6c0595283da3bff0e5a5
unknown <mudit.satija@scryanalytics.com>
2022-01-14 21:22:19 +0530

    sprint19:  nuxeoServerUrl added

--- src/environments/environment.uat.ts
+++ src/environments/environment.uat.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
     apiServiceBaseUrl: 'https://uatgroundx.neom.com',
+    nuxeoServerUrl: 'https://uatgroundx.neom.com',
     apiVersion: '/nuxeo/api/v1'
   };
   
