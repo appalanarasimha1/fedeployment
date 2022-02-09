9e25a0dddd1003fa31e7540cf4795b85971be746
unknown <mudit.satija@scryanalytics.com>
2022-02-09 13:21:16 +0530

    sprin21: no-content page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { TermsOfUseComponent } from './common/terms-of-use/terms-of-use.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LoginComponent } from './login/login.component';
+import { NoContent } from './no-content';
 
 
 const routes: Routes = [
   {path: '', loadChildren: () => import('./search/search.module').then(m => m.SearchModule)},
   {path: 'login', component: LoginComponent},
   {path: 'workspace', loadChildren: () => import('./browse/browse.module').then(m => m.BrowseModule)},
-  {path: 'common', loadChildren: () => import('./common/common-module/common.module').then(m => m.CommonModule)}
+  {path: 'common', loadChildren: () => import('./common/common-module/common.module').then(m => m.CommonModule)},
+  {path: '404', component: NoContent}
 ];
 
 @NgModule({
