44f7109edce3ce1c8a33731b8eec4bd28d54b071
unknown <mudit.satija@scryanalytics.com>
2022-02-10 16:18:31 +0530

    sprnt21: reports in progress

--- src/app/report/report.module.ts
+++ src/app/report/report.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { ReportMainComponent } from './main/main.component';
 import { CardComponent } from './card/card.component';
 import { GraphComponent } from './graph/graph.component';
+import { ReportRoutingModule } from './report-routing.module';
 
 
 
 @NgModule({
   declarations: [ReportMainComponent, CardComponent, GraphComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReportRoutingModule
   ]
 })
 export class ReportModule { }
