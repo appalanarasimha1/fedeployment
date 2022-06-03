0c61f71a4982be5bcc083859c8e918815e4bbe6c
sc-umebha <umesh.bhati@scryanalytics.com>
2022-06-03 18:03:13 +0530

    fix checkbox show issue

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   apiVersion: "/nuxeo/api/v1",
-  apiServiceBaseUrl: "https://uatgroundx.neom.com",
+  // apiServiceBaseUrl: "https://uatgroundx.neom.com",
+  apiServiceBaseUrl: "http://localhost:8090",
   nuxeoServerUrl: 'http://localhost:8090',
   keycloakConfig: {
     url: "https://groundx.the-it-cloud.net/auth/",
