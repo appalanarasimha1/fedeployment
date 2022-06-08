39f80f47089a4d0dc08a5ec154551333288d7daf
sc-mudsat <mudit.satija@scryanalytics.com>
2022-06-08 13:57:25 +0530

    sprint30: elastic db url for oci added

--- server/config/appConfig.oci-prod.ts
+++ server/config/appConfig.oci-prod.ts
@@ -13,7 +13,7 @@ export class AppConfigProduction {
         AUDIT_TABLE: 'audit'
       },
       domain: 'https://groundx.oci.sense.neomos.online/',
-      elasticDbUrl: 'http://10.101.21.63:9700',
+      elasticDbUrl: 'http://10.149.49.21:9200,10.149.49.22:9200,10.149.49.18:9200',
       // elasticDbUrl: 'https://10.101.21.140:9200'
     };
   
