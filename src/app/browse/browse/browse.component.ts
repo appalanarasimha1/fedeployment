9bb85eec2251cb55a1acec50a9d5461daccef031
sc-mudsat <mudit.satija@scryanalytics.com>
2022-07-18 13:58:18 +0530

    sprint33: redirection loop starts when navigation is done continuously fix

--- src/app/browse/browse/browse.component.ts
+++ src/app/browse/browse/browse.component.ts
@@ -170,10 +170,9 @@ export class BrowseComponent implements OnInit, AfterViewInit {
   ];
   searchInitialised: any;
 
-  // routeParams = {
-  //   sector: "",
-  //   folder: "",
-  // };
+  routeParams = {
+    folderId: ""
+  };
   breadcrrumb = `/${WORKSPACE_ROOT}`;
   showFolder = false;
 
@@ -182,7 +181,7 @@ export class BrowseComponent implements OnInit, AfterViewInit {
     this.route.queryParams.subscribe(async (params) => {
       this.loading = true;
       this.searchInitialised = null;
-      // this.routeParams.folder = params.folder;
+      this.routeParams.folderId = params.folder;
 
       if (params.folder && params.folder !== ROOT_ID) {
         this.fetchAllSectors();
@@ -1421,6 +1420,9 @@ export class BrowseComponent implements OnInit, AfterViewInit {
   }
 
   navigateToWorkspaceFolder(uid: string) {
+    if(this.routeParams.folderId === uid) {
+      return;
+    }
     this.router.navigate([ASSET_TYPE.WORKSPACE], { queryParams: { folder: uid } });
   }
 
