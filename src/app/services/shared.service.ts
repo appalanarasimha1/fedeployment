0efae4cc129d066eb391abf78fecf6070800b5ec
sc-mudsat <mudit.satija@scryanalytics.com>
2022-08-09 13:27:23 +0530

    jsencrypt module issue fix

--- src/app/services/shared.service.ts
+++ src/app/services/shared.service.ts
@@ -4,7 +4,7 @@ import { Moment } from 'moment'; // for interface
 import { startCase, camelCase, isEmpty, pluck } from 'lodash';
 import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
-import JSEncrypt from 'JSEncrypt';
+import JSEncrypt from 'jsencrypt';
 import { ASSET_TYPE, EXTERNAL_USER, EXTERNAL_GROUP_GLOBAL, localStorageVars } from '../common/constant';
 import { ApiService } from './api.service';
 import { apiRoutes } from "src/app/common/config";
