22928f1eef01bbd125ecad1351546b983f398a9d
sc-mudsat <mudit.satija@scryanalytics.com>
2022-08-12 16:38:37 +0530

    sprint34: logout once-again

--- src/app/services/http-interceptor.service.ts
+++ src/app/services/http-interceptor.service.ts
@@ -6,35 +6,31 @@ import {
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
 import { tap } from 'rxjs/operators';
-// import { HttpResponse } from '@angular/common/http';
+import { SharedService } from './shared.service';
+import { KeycloakService } from 'keycloak-angular';
+import { NuxeoService } from './nuxeo.service';
 
 /** Pass untouched request through to the next request handler. */
 @Injectable()
 export class InterceptorService implements HttpInterceptor {
-  constructor(private router: Router) { }
+  constructor(
+    public nuxeo: NuxeoService,
+    protected readonly keycloak: KeycloakService
+  ) { }
   intercept(req: HttpRequest<any>, next: HttpHandler):
     Observable<HttpEvent<any>> {
       req = req.clone(
         {setHeaders: { 'X-Authentication-Token': localStorage.getItem('token') }
       });
-    return next.handle(req)
-        // .pipe(
-        //     tap((httpEvent: any) =>{
-        //         // Skip request
-        //         if(httpEvent.type === 0){
-        //             return;
-        //         }           
-        //         console.log("response: ", httpEvent.headers);  
-
-        //         let minTargetApiVersion : string;
-        //         if (httpEvent instanceof HttpResponse) {
-        //             if(httpEvent.headers.has('mintargetapiversion')) {
-        //                 minTargetApiVersion = httpEvent.headers.get('mintargetapiversion');
-        //                 console.log("qwsdfghjmklopoiuytrewsxcvbnjuytfd",minTargetApiVersion);
-                        
-        //             }
-        //         }
-        //     })
-    // )
-  }
+      
+      if(localStorage.getItem("logout-once-again")) {
+        return next.handle(req);
+      } else {
+        localStorage.setItem("logout-once-again", "true");
+        localStorage.removeItem('Administrator-default-nuxeo-recent-documents');
+        this.nuxeo.logout();
+        this.keycloak.logout(window.location.origin + '/login');
+        return next.handle(req);
+      }
+    }
 }
