58671b152fd70e2567633d2897f8f29eb143e925
sc-mudsat <mudit.satija@scryanalytics.com>
2022-10-03 14:07:43 +0530

    sprint38: video card preview issue fix

--- proxy.conf.js
+++ proxy.conf.js
@@ -7,7 +7,7 @@ const PROXY_CONFIG = [
         "target":
         // "https://groundx.neom.com",
         //  "http://localhost:8080",
-        "https://uatgroundx.neom.com", //"https://34.219.179.33:5050",  'https://uatgroundx.neom.com',///
+        "https://groundx.neom.com", //"https://34.219.179.33:5050",  'https://uatgroundx.neom.com',///
         "secure": false,
         "changeOrigin": true
     }];
