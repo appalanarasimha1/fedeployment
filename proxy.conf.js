2cec33257ac83ca6244089f944dc1ad7433900f6
sc-mudsat <mudit.satija@scryanalytics.com>
2022-10-12 14:54:04 +0530

    sprint39: proxy url changed to oci-uat

--- proxy.conf.js
+++ proxy.conf.js
@@ -4,14 +4,7 @@ const PROXY_CONFIG = [
             "/nuxeo/",
             "/sockjs-node/",
         ],
-<<<<<<< HEAD
         "target":"https://uatgroundx.oci.sense.neomos.online", // 'https://uatgroundx.neom.com', 
-=======
-        "target":
-        // "https://groundx.neom.com",
-        //  "http://localhost:8080",
-        "https://uatgroundx.oci.sense.neomos.online", //"https://34.219.179.33:5050",  'https://uatgroundx.neom.com',///
->>>>>>> origin/umesh_sprint38
         "secure": false,
         "changeOrigin": true
     }];
