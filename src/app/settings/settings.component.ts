8308768584e53feb172bb370e0ed65c6aee8b4fa
daolai <dao@codelink.io>
2022-12-23 16:21:52 +0700

    GX-3386 Check report role for manage supplier and region

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -6,6 +6,7 @@ import { apiRoutes } from "src/app/common/config";
 import { SharedService } from "src/app/services/shared.service";
 import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
 import { AddUserPrivateFolderModalComponent } from '../add-user-private-folder-modal/add-user-private-folder-modal.component';
+import { REPORT_ROLE } from '../common/constant';
 
 @Component({
   selector: 'app-settings',
@@ -198,5 +199,10 @@ export class SettingsComponent implements OnInit {
     this.showUserAccessPage = false;
     this.showUserManageSuppliers = false;
     this.loading = false;
+  }
+
+  checkReportRole() {
+    const expectedRole = REPORT_ROLE;
+    return this.sharedService.chekForReportRoles(expectedRole);
   }
 }
