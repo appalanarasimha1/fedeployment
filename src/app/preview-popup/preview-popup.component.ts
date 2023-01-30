0153cc8560c1d9efe7f100894b98755a0d002333
sc-umebha <umesh.bhati@neom.com>
2023-01-30 17:07:51 +0530

    GX-3456: magnifier (zoom-in) when it hovers on preview area

--- src/app/preview-popup/preview-popup.component.ts
+++ src/app/preview-popup/preview-popup.component.ts
@@ -622,11 +622,23 @@ export class PreviewPopupComponent implements OnInit, OnChanges {
     return temp.replaceAll("}}", "</span>");
   }
 
-  clickFullsizeImg() {
+  clickFullsizeImg(event) {
+    event.stopPropagation()
     this.fullSIzeImg = !this.fullSIzeImg;
   }
 
   isFavorite() {
     return this.doc.contextParameters.favorites?.isFavorite;
+  }
+
+  zoom(e) {
+    // e.stopPropagation();
+    var zoomer = e.currentTarget;
+    let offsetX, offsetY, x, y;
+    e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX;
+    e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX;
+    x = offsetX/zoomer.offsetWidth*100;
+    y = offsetY/zoomer.offsetHeight*100;
+    zoomer.style.backgroundPosition = x + '% ' + y + '%';
   }
 }
