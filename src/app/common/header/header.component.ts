7fbcfeeccc8c50c7a123af06bda1305588b7e096
sc-mudsat <mudit.satija@scryanalytics.com>
2023-03-21 14:10:51 +0530

    sprint50: gx-3879 done

--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -102,10 +102,13 @@ export class HeaderComponent implements OnInit {
           this.missingHeader = false;
         }
         if (event.url.includes('documentation-assets')) {
-          this.isDroneUploadPage = true;
-        } else {
-          this.isDroneUploadPage = false;
-        }
+          // this.isDroneUploadPage = true;
+          this.showFooter = true;
+          this.isDroneUploader = false;
+        } 
+        // else {
+        //   this.isDroneUploadPage = false;
+        // }
 
       }
     });
