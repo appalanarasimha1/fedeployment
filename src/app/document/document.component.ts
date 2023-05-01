562ccb846f2ea8d61a504bc4c72b14497b3abc25
sc-mudsat <mudit.satija@scryanalytics.com>
2023-05-01 14:11:35 +0530

    sprint53: bracket error fix

--- src/app/document/document.component.ts
+++ src/app/document/document.component.ts
@@ -535,7 +535,6 @@ export class DocumentComponent implements OnInit, OnChanges {
         this.loading.pop();
       })
       .catch((error) => {
-        if(error?.response?.status === 403) {
         this.loading.pop();
          if(error?.response?.status === 403) {
           this.excludedDroneWorkspaces = "";
