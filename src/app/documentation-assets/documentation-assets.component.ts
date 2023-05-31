bcf3cd1da8c80856c21f2e295e008030b2395c19
sc-nitran <101239405+sc-nitran@users.noreply.github.com>
2023-05-31 18:30:26 +0530

    date string function changed

--- src/app/documentation-assets/documentation-assets.component.ts
+++ src/app/documentation-assets/documentation-assets.component.ts
@@ -361,7 +361,7 @@ export class DocumentationAssetsComponent implements OnInit {
   }
 
   formatDateString(date) {
-    return date.toISOString().split("T")[0];
+    return new Date(date.getTime() - (date.getTimezoneOffset() * 60000)).toISOString().split("T")[0];
   }
 
   openModal() {
